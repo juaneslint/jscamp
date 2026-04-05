6f922507f650d3047c96f814ecadcfe0ad6dec07
Juan Esteban Martínez <jei.martinezm@gmail.com>
2026-04-05 19:08:14 -0400

    Se añade componente JobListing

---
+++ 02-react/src/components/JobListings.jsx
@@ -0,0 +1,8 @@
+export default function JobListings() {
+    return (
+        <>
+            <h2>Resultados de búsqueda</h2>
+            <div className="jobs-listings"></div>
+        </>
+    )
+}
