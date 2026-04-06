59f828d0c71a0e1ed620eac9ad5ebf280b157d12
Juan Esteban Martínez <jei.martinezm@gmail.com>
2026-04-05 22:14:17 -0400

    Hook UseId()

--- 02-react/src/components/SearchFormSection.jsx
+++ 02-react/src/components/SearchFormSection.jsx
@@ -1,10 +1,20 @@
+import { useId } from "react";
+
 function SearchFormSection() {
+    const idText = useId()
+    const idTechnology = useId()
+    const idLocation = useId()
+    const idExperienceLevel = useId()
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+    }
     return (
         <section className="jobs-search">
             <h1>Encuentra tu próximo trabajo</h1>
             <p>Explora miles de oportunidades en el sector tecnológico.</p>
 
-            <form role="search">
+            <form onSubmit={handleSubmit} role="search">
                 <div className="search-bar">
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
                         stroke="currentColor" strokeWidth="1" strokeLinecap="round" strokeLinejoin="round"
@@ -14,11 +24,12 @@ function SearchFormSection() {
                         <path d="M21 21l-6 -6" />
                     </svg>
 
-                    <input required type="text" placeholder="Buscar trabajos, empresas o habilidades" />
+                    <input name={idText} required type="text" placeholder="Buscar trabajos, empresas o habilidades" />
+                    <button type="submit" style={{ position: 'absolute', right: '4px' }}>Buscar</button>
                 </div>
 
                 <div className="search-filters">
-                    <select name="technology" id="filter-technology">
+                    <select name={idTechnology} id="filter-technology">
                         <option value="">Tecnología</option>
                         <option value="javascript">JavaScript</option>
                         <option value="python">Python</option>
@@ -27,7 +38,7 @@ function SearchFormSection() {
                         <option value="nodejs">Node.js</option>
                     </select>
 
-                    <select name="location" id="location">
+                    <select name={idLocation} id="location">
                         <option value="">Ubicación</option>
                         <option value="remoto">Remoto</option>
                         <option value="cdmx">Ciudad de México</option>
@@ -36,7 +47,7 @@ function SearchFormSection() {
                         <option value="barcelona">Barcelona</option>
                     </select>
 
-                    <select name="experience-level" id="experience-level">
+                    <select name={idExperienceLevel} id="experience-level">
                         <option value="">Nivel de experiencia</option>
                         <option value="junior">Junior</option>
                         <option value="mid">Mid-level</option>
