68ec49d145b80bc29428ecd1a0e9c3fbbc5d0bd6
Juan Esteban Martínez <jei.martinezm@gmail.com>
2026-04-18 18:54:50 -0400

    Se añade paginación a la búsqueda de trabajos al implementar límites y desplazamientos en la API.

--- 02-react/src/pages/Search.jsx
+++ 02-react/src/pages/Search.jsx
@@ -30,6 +30,10 @@ const useFilters = () => {
                 if(filters.location) params.append('type', filters.location)
                 if(filters.experienceLevel) params.append('level', filters.experienceLevel)
                 
+                const offset = (currentPage - 1) * RESULTS_PER_PAGE
+                params.append('limit', RESULTS_PER_PAGE)
+                params.append('offset', offset)
+
                 const queryParams = params.toString()
 
                 const response = await fetch(`https://jscamp-api.vercel.app/api/jobs?${queryParams}`)
@@ -49,7 +53,7 @@ const useFilters = () => {
 
    
 
-    const totalPages = Math.ceil(jobs.length / RESULTS_PER_PAGE)
+    const totalPages = Math.ceil(total / RESULTS_PER_PAGE)
 
     const handlePageChange = (page) => {
         setCurrentPage(page);
